662cca7447b97f9d873e5259d6f8b246fdacef68
Josh Ford <thisisjoshford@gmail.com>
2020-03-09 13:12:49 -0700

    isString function & tests working

---
+++ __tests__/types.test.js
@@ -0,0 +1,44 @@
+const {
+  isNumber,
+  isString,
+  castToNumber,
+  getCaster
+} = require('../lib/types.js');
+
+describe('validator module', () => {
+  describe('basic validation', () => {
+    it('properly tells if a value is a numbers', () => {
+      expect(isNumber(3)).toBeTruthy();
+      expect(isNumber('hi')).toBeFalsy();
+      expect(isNumber([])).toBeFalsy();
+      expect(isNumber({})).toBeFalsy();
+      expect(isNumber(() => {})).toBeFalsy();
+      expect(isNumber(true)).toBeFalsy();
+      expect(isString('hello')).toBeTruthy();
+      expect(isString(3)).toBeFalsy();
+      expect(isString({})).toBeFalsy();
+      expect(isString('yooooooo')).toBeTruthy();
+      expect(isString('100')).toBeTruthy();
+      expect(isString([])).toBeFalsy();
+    });
+  });
+
+  describe('casters', () => {
+    it('can cast values to a number', () => {
+      expect(castToNumber(3)).toEqual(3);
+      expect(castToNumber('3')).toEqual(3);
+      expect(castToNumber(true)).toEqual(1);
+      expect(castToNumber(false)).toEqual(0);
+    });
+
+    it('throws if value is not castable to number', () => {
+      expect(() => castToNumber('hi')).toThrowErrorMatchingSnapshot();
+      expect(() => castToNumber({})).toThrowErrorMatchingSnapshot();
+    });
+  });
+
+  it('can get the right caster', () => {
+    expect(getCaster(Number)).toEqual(castToNumber);
+    expect(getCaster(Promise)).toBeNull();
+  });
+});
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const { isNumber } = require('./lib/types.js');
 
 console.log(isNumber('3'));
+
+console.log(isNumber(2));
