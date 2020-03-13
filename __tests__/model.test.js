d287ed4783e5b3dbdffa4232d6b0389f42ef8669
Josh Ford <thisisjoshford@gmail.com>
2020-03-13 16:47:02 -0700

    findByIDandUpdate working

--- __tests__/model.test.js
+++ __tests__/model.test.js
@@ -35,4 +35,42 @@ describe('Model class', () => {
       });
   });
 
+  it('finds by id and updates', () => {
+    const schema = new Schema({
+      name: {
+        type: String,
+        required: true
+      },
+      age: {
+        type: Number,
+        required: true
+      },
+      weight: {
+        type: String
+      }
+    });
+
+    const Dog = new Model('Dog', schema);
+
+    return Dog
+      .create({
+        name: 'spot',
+        age: 5,
+        weight: '20 lbs'
+      })
+      .then(dog => {
+        return Dog
+          .findByIdAndUpdate(dog._id, { name: 'rover' });
+      })
+      .then(updatedDog => {
+        expect(updatedDog).toEqual({
+          _id: expect.any(String),
+          name: 'rover',
+          age: 5,
+          weight: '20 lbs'
+        });
+      });
+  });
+
+
 });
